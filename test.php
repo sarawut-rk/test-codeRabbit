c904f8ae08eb1788ff3cf3bc9b0f86b8c9cc009b
donyaritm-sketch <donyarit.m@zeabix.com>
2026-09-02 07:33:23 +0000

    add comment

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
  * Simple standalone PHP test script.
  * Run with: php test.php
  */
-
+//test
 function test(string $label, mixed $actual, mixed $expected): void
 {
     $pass = $actual === $expected;
